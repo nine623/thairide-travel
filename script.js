c61bd8027fd2eb3f59dcb9d41d88150b4069004a
nine623 <94023920+nine623@users.noreply.github.com>
2026-03-05 09:06:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
-function updateClock(){
+function updateTime(){
+let now = new Date();
 
-const now = new Date();
+let date = now.toLocaleDateString("th-TH");
+let time = now.toLocaleTimeString("th-TH");
 
-const date = now.toLocaleDateString("th-TH");
-const time = now.toLocaleTimeString("th-TH");
-
-document.getElementById("date").innerHTML="📅 "+date;
-document.getElementById("time").innerHTML="⏰ "+time;
-
+document.getElementById("datetime").innerHTML =
+"วันที่ " + date + " | เวลา " + time;
 }
 
-setInterval(updateClock,1000);
-
-function sendToLine(){
-
-let name=document.getElementById("name").value;
-let date=document.getElementById("dateTravel").value;
-let time=document.getElementById("timeTravel").value;
-let pickup=document.getElementById("pickup").value;
-let drop=document.getElementById("drop").value;
-let car=document.getElementById("car").value;
-let people=document.getElementById("people").value;
-let note=document.getElementById("note").value;
-
-let msg=
-`จองรถ ThaiRide Travel
-
-ชื่อ: ${name}
-วันที่: ${date}
-เวลา: ${time}
-
-รับ: ${pickup}
-ส่ง: ${drop}
-
-รถ: ${car}
-ผู้โดยสาร: ${people}
-
-หมายเหตุ: ${note}
-`;
-
-let url="https://line.me/R/share?text="+encodeURIComponent(msg);
-
-window.open(url);
-}
+setInterval(updateTime,1000);
