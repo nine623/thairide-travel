fea35e6d504200c5f5d8e9a44e3fd6def6c99c64
nine623 <94023920+nine623@users.noreply.github.com>
2026-03-06 12:09:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+
+// CLOCK
+
 function updateClock(){
 
-let now = new Date()
+let now=new Date()
 
-let time = now.toLocaleTimeString()
-let date = now.toDateString()
+document.getElementById("time").innerHTML=
+now.toLocaleTimeString()
 
-document.getElementById("clock").innerHTML = time
-document.getElementById("date").innerHTML = date
+document.getElementById("date").innerHTML=
+now.toDateString()
 
 }
 
 setInterval(updateClock,1000)
-updateClock()
+
+
+
+// LINE BOOKING
+
+document.getElementById("bookingForm").addEventListener("submit",function(e){
+
+e.preventDefault()
+
+let text="Booking ThaiRide%0A"
+
+window.open("https://line.me/R/ti/p/@775hjmvl?text="+text)
+
+})
+
+
+// VISITOR COUNTER
+
+let online=Math.floor(Math.random()*50)+20
+let today=Math.floor(Math.random()*500)+100
+
+document.getElementById("online").innerHTML=online
+document.getElementById("today").innerHTML=today
