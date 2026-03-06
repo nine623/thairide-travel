76381e874b82cc3f7c3b5a7c949a0834203ec698
nine623 <94023920+nine623@users.noreply.github.com>
2026-03-06 12:47:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,13 @@
-
-// CLOCK
-
 function updateClock(){
 
-const now=new Date()
+const now = new Date();
 
-document.getElementById("time").innerHTML=now.toLocaleTimeString()
-document.getElementById("date").innerHTML=now.toDateString()
+document.getElementById("time").innerHTML =
+now.toLocaleTimeString();
 
-}
-
-setInterval(updateClock,1000)
-
-
-// HERO SLIDE
-
-let slide=0
-const slides=document.querySelectorAll(".hero-slide img")
-
-function showSlide(){
-
-slides.forEach(s=>s.style.display="none")
-
-slide++
-
-if(slide>slides.length) slide=1
-
-slides[slide-1].style.display="block"
+document.getElementById("date").innerHTML =
+now.toDateString();
 
 }
 
-setInterval(showSlide,4000)
-
-showSlide()
-
-
-
-// BOOKING → LINE
-
-function sendLine(){
-
-window.open("https://lin.ee/775hjmvl")
-
-}
-
-
-// LIVE VISITOR
-
-fetch("https://api.countapi.xyz/hit/thairide/visits")
-
-.then(res=>res.json())
-
-.then(res=>{
-
-console.log("Visitors:",res.value)
-
-})
+setInterval(updateClock,1000);
